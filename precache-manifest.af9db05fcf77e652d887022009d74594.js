08f94ebdbb73d056c482ca8a65533f774a00367f
chhotu05 <chhotukumar20102000@gmail.com>
2019-03-24 22:42:37 +0530

    Updates

---
+++ precache-manifest.af9db05fcf77e652d887022009d74594.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a8680c80c11bd3c260e",
+    "url": "/Todo-app-/static/js/runtime~main.cc566a98.js"
+  },
+  {
+    "revision": "4a68d868a0481d32ddff",
+    "url": "/Todo-app-/static/js/main.059a3db4.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/Todo-app-/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "4a68d868a0481d32ddff",
+    "url": "/Todo-app-/static/css/main.ef2591e4.chunk.css"
+  },
+  {
+    "revision": "4d2abd356a4ace379156b6f5060cdcfb",
+    "url": "/Todo-app-/index.html"
+  }
+];
